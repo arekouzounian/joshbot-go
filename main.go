9329f48e744ccea8534afad5d283c8285de11659
Arek Ouzounian <agouzo777@gmail.com>
2024-04-14 18:21:08 -0700

    fixed message content error, added thorough logging and perms check

--- main.go
+++ main.go
@@ -3,18 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-var Token string
+var (
+	Token   string
+	LogFile string
+)
+
+// hardcoded server ID; allows testing on other server
+// might be a better way to do this
+const (
+	GuildID = "715798257661509743"
+)
 
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.StringVar(&LogFile, "o", "./joshbot.log", "The file to output logs to. By default, creates a file in the current directory named 'joshbot.log'")
 	flag.Parse()
 }
 
@@ -25,21 +36,30 @@ func main() {
 		return
 	}
 
-	dg, err := discordgo.New("Bot " + Token)
+	file, err := os.OpenFile(LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println("Error creating discord session: " + err.Error())
+		log.Fatalf("Error opening logfile: %s could not be opened.", LogFile)
 		return
 	}
+	defer file.Close()
 
-	dg.Identify.Intents = discordgo.IntentGuildMessages
-	//dg.Identify.Intents += discordgo.IntentDirectMessages
+	log.SetOutput(file)
+
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatalf("Error creating discord session: %s\n", err.Error())
+	}
+
+	log.Println("Discord session started")
+
+	// https://discord.com/developers/docs/topics/gateway#gateway-intents
+	dg.Identify.Intents = discordgo.IntentGuildMessages | discordgo.IntentGuildMembers
 
 	dg.AddHandler(messageCreate)
 
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("Error opening discord connection: " + err.Error())
-		return
+		log.Fatalf("Error opening discord connection: %s", err.Error())
 	}
 
 	fmt.Println("Bot running! Use Ctrl-C to exit.")
@@ -48,9 +68,35 @@ func main() {
 	<-sigchannel
 
 	dg.Close()
+
+	log.Printf("Received interrupt, shutting down\n\n")
 }
 
 func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
-	fmt.Printf("[%s] Message Created: %s\n", time.Now().String(), message.Content)
-	//session.ChannelMessageDelete(message.ChannelID, message.ID, )
+	if message.GuildID != GuildID {
+		return
+	}
+
+	if strings.ToLower(message.Content) != "josh" {
+		log.Printf("Non-josh message detected: %s: %s\n", message.Author.Username, message.Content)
+		err := session.ChannelMessageDelete(message.ChannelID, message.ID)
+		if err != nil {
+			log.Printf("Error deleting message: %s", err.Error())
+
+			perms, err := session.State.UserChannelPermissions(session.State.User.ID, message.ChannelID)
+			if err != nil {
+				log.Fatalf("Unable to retreive user channel permissions: %s", err.Error())
+			}
+
+			if perms&discordgo.PermissionAdministrator == 0 {
+				log.Printf("Not running as administrator on this channel! Won't be able to remove messages.")
+				session.ChannelMessageSend(message.ChannelID, "I need to be admin to work effectively.")
+			}
+
+		} else {
+			log.Printf("Message deleted successfully.")
+		}
+	} else {
+		// handle josh message !
+	}
 }
